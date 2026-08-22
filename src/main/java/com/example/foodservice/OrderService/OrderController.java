b55e190f5f9c3a7ab41409a78c456c259ab84242
kira <sanyasatana@yandex.ru>
2026-08-23 01:06:33 +0300

    feat(order): add incrementItemQuantity and addItem methods in OrderController

--- src/main/java/com/example/foodservice/OrderService/OrderController.java
+++ src/main/java/com/example/foodservice/OrderService/OrderController.java
@@ -1,24 +1,24 @@
 package com.example.foodservice.OrderService;
 
 
-import com.example.foodservice.OrderService.dto.CreateOrderInfo;
-import com.example.foodservice.OrderService.dto.CreateOrderRequest;
-import com.example.foodservice.OrderService.dto.CreateOrderResponse;
-import com.example.foodservice.OrderService.dto.OrderItemsRequest;
+import com.example.foodservice.OrderService.dto.*;
 import com.example.foodservice.common.ResponseBuilder;
 import com.example.foodservice.common.dto.ApiResponse;
+import jakarta.persistence.PostUpdate;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import jakarta.websocket.server.PathParam;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "order")
+@RequestMapping(value = "/order")
 @RequiredArgsConstructor
 public class OrderController {
     private final OrderService orderService;
@@ -31,5 +31,30 @@ public class OrderController {
         CreateOrderInfo orderInfo = orderService.createOrder(request.toCommand(userId));
 
         return responseBuilder.created(CreateOrderResponse.from(orderInfo));
+    }
+
+    @Validated
+    @PostMapping(value = "/add-item")
+    public ResponseEntity<ApiResponse<OrderInfo>> addItem(
+            @Positive @RequestParam Long restaurantId,
+            @Positive @RequestParam Long menuItemId,
+            @AuthenticationPrincipal Long userId) {
+
+        AddItemCommand command = AddItemCommand.from(userId, restaurantId, menuItemId);
+
+        OrderInfo orderInfo = orderService.addItem(command);
+
+        return responseBuilder.ok(orderInfo);
+    }
+    @PatchMapping(value = "/items/{orderItemId}/increment")
+    public ResponseEntity<ApiResponse<Void>> incrementItemQuantity(
+            @PathVariable Long orderItemId,
+            @AuthenticationPrincipal Long userId) {
+
+        ItemIncrementCommand command = ItemIncrementCommand.from(userId, orderItemId);
+
+        orderService.incrementItemQuantity(command);
+
+        return responseBuilder.ok(null);
     }
 }
