9c4131e33513ce24bb7325378791d67bad359d1b
kira <sanyasatana@yandex.ru>
2026-08-23 01:07:20 +0300

    feat(order): add findByUserIdAndRestaurantId method in OrderRepository

--- src/main/java/com/example/foodservice/OrderService/OrderRepository.java
+++ src/main/java/com/example/foodservice/OrderService/OrderRepository.java
@@ -3,6 +3,9 @@ package com.example.foodservice.OrderService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByUserIdAndRestaurantId(Long userId, Long restaurantId);
 }
